9b57b5e27c02a75056ee3a097a1df151a23d5287
Vikrant Negi <Vikrant.negi74@gmail.com>
2025-02-21 03:20:04 +0530

    setup

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Vaibhav Negi | 3D Animator",
+  description:
+    "A Passionate 3D Animator, experienced in Characters Creation, Environment Building, Modelling and Rigging!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
